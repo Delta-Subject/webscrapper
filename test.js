aded6c3df49b3edbbd5d4bf910db10144d2b6374
Delta Subject <veradjvg2332@gmail.com>
2022-04-19 15:31:08 -0400

    First Stable Version

---
+++ test.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
+
+const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const parser = 'td > b > a[title]';
+
+// Functioning
+
+const response = await fetch(url);
+const body = await response.text();
+
+const $ = cheerio.load(body);
+const output = $(parser).text();
+
+// Debugging
+
+console.log(output);
