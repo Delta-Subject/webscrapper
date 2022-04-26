4277f3b4b315cc2f7d05e39a3f02bc6a29d46fc5
Delta Subject <veradjvg2332@gmail.com>
2022-04-26 14:39:13 -0400

    Refactorized Exception Handling. Wrapped Inside A Single Less-Bloated Function

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
 import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
-const parser = 'td > b > a[title]';
 
-// Functioning
-
-let response, body, $;
-const output = [];
-
-try {
-    response = await fetch(url);
-    body = await response.text();
-    $ = cheerio.load(body);
-    $(parser).each((i, elem) => output[i] = $(elem).text());
-} catch (error) {
-    console.log(error);
+const scrape = async (url, parser) => {
+    try {
+        const output = []
+        const $ = cheerio.load(await fetch(url).then((res) => res.text()));
+        $(parser).each((i, elem) => output[i] = $(elem).text());
+        return output;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
 // Debugging
 
-console.log(output);
+console.log(await scrape('https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States', 'td > b > a[title]'))
