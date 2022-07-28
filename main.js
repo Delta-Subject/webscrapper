f5e1db5ca14c36d4f1c6edc539ecdc533f33766a
Diosmer Vera <veradjvg2332@gmail.com>
2022-07-28 11:27:21 -0400

    Added StandardJS Style into code and finally started to write some files

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-import fetch from 'node-fetch';
-import * as cheerio from 'cheerio';
-
+import fetch from 'node-fetch'
+import * as cheerio from 'cheerio'
+import fs from 'fs'
 
 const scrape = async (url, parser) => {
-    try {
-        const output = []
-        const $ = cheerio.load(await fetch(url).then((res) => res.text()));
-        $(parser).each((i, elem) => output[i] = $(elem).text());
-        return output;
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const output = []
+    const $ = cheerio.load(await fetch(url).then((res) => res.text()))
+    $(parser).each((i, elem) => (output[i] = $(elem).text()))
+    return output
+  } catch (error) {
+    console.log(error)
+  }
 }
 
+const data = await scrape('https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States', 'td > b > a[title]').then((res) => res.toString())
 
+fs.writeFile('output.txt', data, (err) => { if (err) { console.err(err) } })
+
+console.log('Succesfully wrote ' + data + ' into output.txt')
 // Debugging
-
-console.log(await scrape('https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States', 'td > b > a[title]'))
+// console.log(data)
